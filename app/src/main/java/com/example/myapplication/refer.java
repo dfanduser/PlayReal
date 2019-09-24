744b7b31b9bb5da075b865ae8082a024d6d58f57
dfanduser <dfanduser@gmail.com>
2019-09-24 12:06:43 +0530

    Dialog box customization added

--- app/src/main/java/com/example/myapplication/refer.java
+++ app/src/main/java/com/example/myapplication/refer.java
@@ -3,11 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Random;
 
 public class refer extends AppCompatActivity {
 
